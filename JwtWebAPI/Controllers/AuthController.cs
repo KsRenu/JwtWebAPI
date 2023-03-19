2bc26b9baa32bd7a0102c73da71221fae810b556
Renu Konda <renuks0802@gmail.com>
2023-03-19 11:30:49 +0530

    implementing the cryptographic algorithm

--- JwtWebAPI/Controllers/AuthController.cs
+++ JwtWebAPI/Controllers/AuthController.cs
@@ -3,45 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace JwtWebAPI.Controllers
 {
     [Route("api/[controller]")]
+
     public class AuthController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        public static User user = new User();
+
+        [HttpPost("register")]
+
+        public async Task<ActionResult<User>> Register(UserDto request)
         {
-            return new string[] { "value1", "value2" };
+        }
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
 
