15a4db69bc1a1fa81b5b2aa790c4ac3fe9b94f82
Renu Konda <renuks0802@gmail.com>
2023-03-19 11:35:29 +0530

    creating password hash

--- JwtWebAPI/Controllers/AuthController.cs
+++ JwtWebAPI/Controllers/AuthController.cs
@@ -19,9 +19,21 @@ namespace JwtWebAPI.Controllers
 
         public async Task<ActionResult<User>> Register(UserDto request)
         {
+            CreatePasswordHash(request Password, out byte[] passwordHash, out byte[] passwordSalt );
+
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
         }
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputerHash(System.Text.Encoding.UTF8.GetBytes(password))
+            }
 
         }
 
